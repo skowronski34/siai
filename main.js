b5c760eda6193924de187be8afe26d3c0b729d26
skowronski34 <gskowronski34@gmail.com>
2024-11-28 15:03:24 +0000

    podstawa

---
+++ main.js
@@ -0,0 +1,16 @@
+let podsiad = true;
+    console.log(podsiad)
+    if(podsiad){
+        console.log("** Smiec **");
+    }
+    else {
+        console.log("Spoko gosciu");
+} 
+    let dawid = false;
+    console.log(dawid)
+    if(dawid){
+        console.log("** Smiec **");
+    }
+    else {
+        console.log("Spoko gosciu");
+}   
